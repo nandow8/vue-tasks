4ae180640594a5bb92a1b31f91face7fcd4e91ca
Fernando Alves <alvesfernandosantos1@gmail.com>
2019-02-24 08:58:32 -0300

    component com parametros

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
 <template>
 	<div id="app">
 		<h1>Tarefas</h1>
+		<TaskGrid  :tasks="tasks"/>
 	</div>
 </template>
 
 <script>
-export default {
+import TaskGrid from './components/TaskGrid.vue'
 
+export default {
+	components: {TaskGrid},
+	data(){
+		return{
+			tasks: [
+				{ name: 'Lavar a louça', pending: false},
+				{ name: 'Comprar blusa', pending: true}
+			]
+		}
+	}
 }
 </script>
 
